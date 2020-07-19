72f46923657cd7b8677f702926f06c7c5c73fb1b
steffanynaranjov <1792@holbertonschool.com>
2020-07-19 22:16:08 +0000

    header 8

--- holberton.h
+++ holberton.h
@@ -7,27 +7,27 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include <limits.h>
-/**
-* struct t_input - Struct type to select specifiers
-*
-* @character: the argument passed to the function
-* @ch: pointer to a function
-*
-*/
-typedef struct t_input
-{
-char *character;
-int  (*ch)(vali_list vlist, char *output_p, int o_p);
-} t_f;
 
 int _printf(const char *format, ...);
-int _putchar(char c);
-int (*format_type(char *s))(va_list vlist char *output_p int o_p);
+int (*format_type(char *s))(va_list vlist, char *output_p, int o_p);
 int print_percent(va_list vlist, char *output_p, int o_p);
-void _puts(char *s);
 int print_char(va_list vlist, char *output_p, int o_p);
-int strlen_(char *str)
+int s_trlen(char *str);
 
+/**
+* struct t_format - Struct t_format
+*
+* @character: the class
+* @ch: pointer to a function
+*
+* Description: the structure for the format type
+*/
+typedef struct t_format
+{
+char *character;
+  int (*ch)(va_list vlist, char *output_p, int o_p);
+} t_f;
 
 #endif
