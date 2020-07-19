58ec65cb426c82c09aa82a785e47a753bd56c9f8
Sofiag8 <1943@holbertonschool.com>
2020-07-19 19:02:36 +0000

    header 6

--- holberton.h
+++ holberton.h
@@ -22,5 +22,6 @@ int _putchar(char c);
 int (*format_type(char *s))(va_list vlist char *output_p int o_p);
 int print_percent(va_list vlist, char *output_p, int o_p);
 void _puts(char *s);
+int print_char(va_list vlist, char *output_p, int o_p);
 
 #endif
