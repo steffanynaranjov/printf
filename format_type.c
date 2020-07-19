597a59b8bcd691e1c7af379eee4694087db47cf3
steffanynaranjov <1792@holbertonschool.com>
2020-07-19 17:47:16 +0000

    format type

---
+++ format_type.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+* format_type - type of function
+* @s: input
+*
+* Description: Function to get the type function
+* Return: type or null
+*/
+int (*format_type(char *s))(va_list vlist char *output_p int o_p)
+{
+t_f types[] = {
+{"c", print_char},
+{"s", print_string},
+{"i", print_single_int},
+{"d", print_int},
+{"b", print_binary},
+{"r", print_string_reverse},
+{"R", print_rot13},
+{"u", print_unsignet_int},
+{"o", print_octal},
+{"x", print_hexa},
+{"X", print_unsigned_hexad},
+{"p", print_address},
+{"S", print_the_string},
+{"%", print_percent},
+{NULL, NULL}
+};
+int x = 0;
+
+while (types[x].character)
+{
+if (types[x].character[0] == s[0] && s[0] != '\0')
+return (types[x].ch);
+x++;
+}
+return (NULL);
+}
