7a8564f32816cf78ee67d905aa209aea2b116d40
Sofiag8 <1943@holbertonschool.com>
2020-07-19 17:14:04 +0000

    header 1

---
+++ holberton.h
@@ -0,0 +1,26 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+/**
+ * struc t_input - type of the struct to select specifiers
+ *
+ * @character: the argument passed to the function
+ * @ch: pointer to a function
+ *
+ */
+
+typedef struct t_input
+{
+	char *character;
+	int  (*ch)(vali_list vlist, char *output_p, int p_p);
+}t_f;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int (*format_type(char *s))(va_list vlist char *output_p int o_p);
+
+
+#endif
