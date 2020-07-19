b16c24224c23a74c4c532afcd6cee5dc70d0358e
steffanynaranjov <1792@holbertonschool.com>
2020-07-19 22:39:17 +0000

    printf 1

---
+++ _printf.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+* _printf - Print all this parameters
+* @format: input
+*
+* Description: function that prints output
+*
+* Return: The output character or num
+*/
+int _printf(const char *format, ...)
+{
+int x = 0, o_p = 0;
+char *print = (char *) format. *output_p;
+int (*print_ch)(va_list, char *, int);
+va_list vlist;
+
+if (!format)
+return (-1);
+va_start(vlist, format);
+output_p = malloc(sizeof(char) * SIZE);
+if (!output_p)
+return (1);
+while (format[x])
+{
+if (format[x] != '%')
+output_p[o_p] = format[x], o_p++;
+else if (strlen_(print) != 1)
+{
+print_ch = format_type(++print);
+if (!print_ch)
+output_p[o_p] = format[x], o_p++;
+else
+o_p = print_ch(vlist, output_p, o_p), x++;
+}
+else
+o_p = -1;
+x++, print++;
+}
+va_end(vlist);
+write(1, output_p, o_p);
+free(output_p);
+return (o_p);
+{
