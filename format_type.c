79c2e4add3832c2879bde06b0622eae518da9753
Sofiag8 <1943@holbertonschool.com>
2020-07-20 00:55:39 +0000

    format_type2

--- format_type.c
+++ format_type.c
@@ -11,17 +11,6 @@ int (*format_type(char *s))(va_list vlist, char *output_p, int o_p)
 t_f types[] = {
 {"c", print_char},
 {"s", print_string},
-{"i", print_single_int},
-{"d", print_int},
-{"b", print_binary},
-{"r", print_string_reverse},
-{"R", print_rot13},
-{"u", print_unsignet_int},
-{"o", print_octal},
-{"x", print_hexa},
-{"X", print_unsigned_hexad},
-{"p", print_address},
-{"S", print_the_string},
 {"%", print_percent},
 {NULL, NULL}
 };
